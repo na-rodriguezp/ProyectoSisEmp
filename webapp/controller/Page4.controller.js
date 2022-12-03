e7def43e7e69ca123a1fcaaccae4158d13162a43
Nicolas <na.rodriguezp@uniandes.edu.co>
2022-12-02 20:05:54 -0500

    Proyecto

---
+++ webapp/controller/Page4.controller.js
@@ -0,0 +1,181 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.untitledPrototypeCopy.controller.Page4", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App637e78fac2951001ccc9d8db";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype" && prop.includes("Set")) {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onFioriListReportTableItemPress: function(oEvent) {
+
+			var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+
+			return new Promise(function(fnResolve) {
+				this.doNavigate("Page5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		doNavigate: function(sRouteName, oBindingContext, fnPromiseResolve, sViaRelation) {
+			var sPath = (oBindingContext) ? oBindingContext.getPath() : null;
+			var oModel = (oBindingContext) ? oBindingContext.getModel() : null;
+
+			var sEntityNameSet;
+			if (sPath !== null && sPath !== "") {
+				if (sPath.substring(0, 1) === "/") {
+					sPath = sPath.substring(1);
+				}
+				sEntityNameSet = sPath.split("(")[0];
+			}
+			var sNavigationPropertyName;
+			var sMasterContext = this.sMasterContext ? this.sMasterContext : sPath;
+
+			if (sEntityNameSet !== null) {
+				sNavigationPropertyName = sViaRelation || this.getOwnerComponent().getNavigationPropertyForNavigationWithContext(sEntityNameSet, sRouteName);
+			}
+			if (sNavigationPropertyName !== null && sNavigationPropertyName !== undefined) {
+				if (sNavigationPropertyName === "") {
+					this.oRouter.navTo(sRouteName, {
+						context: sPath,
+						masterContext: sMasterContext
+					}, false);
+				} else {
+					oModel.createBindingContext(sNavigationPropertyName, oBindingContext, null, function(bindingContext) {
+						if (bindingContext) {
+							sPath = bindingContext.getPath();
+							if (sPath.substring(0, 1) === "/") {
+								sPath = sPath.substring(1);
+							}
+						} else {
+							sPath = "undefined";
+						}
+
+						// If the navigation is a 1-n, sPath would be "undefined" as this is not supported in Build
+						if (sPath === "undefined") {
+							this.oRouter.navTo(sRouteName);
+						} else {
+							this.oRouter.navTo(sRouteName, {
+								context: sPath,
+								masterContext: sMasterContext
+							}, false);
+						}
+					}.bind(this));
+				}
+			} else {
+				this.oRouter.navTo(sRouteName);
+			}
+
+			if (typeof fnPromiseResolve === "function") {
+				fnPromiseResolve();
+			}
+
+		},
+		_onFioriListReportTableUpdateFinished: function(oEvent) {
+			var oTable = oEvent.getSource();
+			var oHeaderbar = oTable.getAggregation("headerToolbar");
+			if (oHeaderbar && oHeaderbar.getAggregation("content")[1]) {
+				var oTitle = oHeaderbar.getAggregation("content")[1];
+				if (oTable.getBinding("items") && oTable.getBinding("items").isLengthFinal()) {
+					oTitle.setText("(" + oTable.getBinding("items").getLength() + ")");
+				} else {
+					oTitle.setText("(1)");
+				}
+			}
+
+		},
+		_onFioriListReportActionButtonPress: function(oEvent) {
+
+			var oTable = oEvent.getSource().getParent().getParent();
+			return new Promise(function(fnResolve, fnReject) {
+				if (oTable instanceof sap.m.Table) {
+					var aItems = oTable.getSelectedItems();
+					aItems.forEach(function(oItem) {
+						oTable.removeItem(oItem);
+					});
+					fnResolve();
+				} else {
+					fnReject(new Error("can't find selected items on provided collection"));
+				}
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onFioriListReportActionButtonPress1: function(oEvent) {
+
+			var oBindingContext = oEvent.getSource().getBindingContext();
+
+			return new Promise(function(fnResolve) {
+
+				this.doNavigate("Page5", oBindingContext, fnResolve, "");
+			}.bind(this)).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		formatFractionDigitFromValue: function(sValue) {
+			var sNumber;
+			if (isNaN(sValue)) {
+				sNumber = sValue;
+			} else {
+				sNumber = Number(sValue).toFixed(2);
+			}
+			return sNumber;
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Page4").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			this.oFilterBar = null;
+			this.oFilterBar = this.getView().byId("ListReportFilterBar");
+			var oBasicSearch = new sap.m.SearchField({
+				showSearchButton: true
+			});
+			this.oFilterBar.setBasicSearch(oBasicSearch);
+
+		}
+	});
+}, /* bExport= */ true);
